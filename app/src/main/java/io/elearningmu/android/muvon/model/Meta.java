e7740960b76d956b9b02dddb46d9e0460411b6b2
Adwi Arifin <adwiarifin@gmail.com>
2018-06-28 14:43:19 +0700

    modify Meta and add Qestion POJO

--- app/src/main/java/io/elearningmu/android/muvon/model/Meta.java
+++ app/src/main/java/io/elearningmu/android/muvon/model/Meta.java
@@ -3,13 +3,43 @@ package io.elearningmu.android.muvon.model;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 public class Meta {
 
     @SerializedName("access")
     @Expose
     public int access;
 
+    @SerializedName("status")
+    @Expose
+    public int status;
+
     @SerializedName("progress")
     @Expose
     public double progress;
+
+    @SerializedName("marks")
+    @Expose
+    public int marks;
+
+    @SerializedName("max")
+    @Expose
+    public int max;
+
+    @SerializedName("questions")
+    @Expose
+    public List<Question> questions = null;
+
+    @SerializedName("auto")
+    @Expose
+    public int auto;
+
+    @SerializedName("retakes")
+    @Expose
+    public int retakes;
+
+    @SerializedName("completion_message")
+    @Expose
+    public String completionMessage;
 }
---
+++ app/src/main/java/io/elearningmu/android/muvon/model/Question.java
@@ -0,0 +1,53 @@
+package io.elearningmu.android.muvon.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Question {
+
+    @SerializedName("type")
+    @Expose
+    public String type;
+
+    @SerializedName("hint")
+    @Expose
+    public String hint;
+
+    @SerializedName("explanation")
+    @Expose
+    public String explanation;
+
+    @SerializedName("content")
+    @Expose
+    public String content;
+
+    @SerializedName("options")
+    @Expose
+    public List<String> options = null;
+
+    @SerializedName("correct")
+    @Expose
+    public String correct;
+
+    @SerializedName("marks")
+    @Expose
+    public int marks;
+
+    @SerializedName("user_marks")
+    @Expose
+    public int userMarks;
+
+    @SerializedName("status")
+    @Expose
+    public int status;
+
+    @SerializedName("marked")
+    @Expose
+    public Object marked;
+
+    @SerializedName("auto")
+    @Expose
+    public int auto;
+}
